08c6e5287c7daaf5a8dfdf4f7fef6d1fa82fe40e
Jatin Undhad <jatinundhad33@gmail.com>
2023-03-04 16:33:01 +0530

    GUI & model Integration has been done.

--- app.py
+++ app.py
@@ -1,20 +1,37 @@
 from flask import Flask, render_template, request
 from model import model
+import numpy as np
+import pandas as pd
 
 app = Flask(__name__)
 
 
-@app.route("/predict", methods=['GET', 'POST'])
+@app.route("/predictRange", methods=['GET', 'POST'])
+def predictRange():
+    if request.method == 'POST':
+        startDate = request.form["startDate"]
+        endDate = request.form["endDate"]
+        model.predictRange(startDate, endDate)
+
+        df = pd.read_csv("./Prediction/prediction.csv")
+        predictedData = df.to_numpy()
+        predictedData = np.delete(predictedData, [0], 1)
+        return render_template("home.html", date="", predictedData=predictedData, len=len(predictedData))
+
+
+@ app.route("/predict", methods=['GET', 'POST'])
 def predict():
     if request.method == 'POST':
         date = request.form['date']
-        return model.predict(date)
+        predictedData = model.predict(date)
+        return render_template("home.html", date=date, predictedData=predictedData, len=0)
 
 
-@app.route("/")
+@ app.route("/")
 def hello_world():
-    return render_template("home.html")
+    return render_template("home.html", date="", predictedData="", len=-1)
 
 
 if __name__ == '__main__':
+    model.createModel()
     app.run(debug=True)
--- model/model.py
+++ model/model.py
@@ -6,26 +6,66 @@ from sklearn.metrics import mean_squared_error
 from math import sqrt
 from datetime import datetime
 
+model = "model"
+lastDateStr = "lastDateStr"
+lastDate = "lastDate"
 
-def predict(date):
 
+def createModel():
+
+    global model
+    global lastDateStr
+    global lastDate
     data = pd.read_csv('./data_daily.csv', index_col="Date", parse_dates=True)
     data.index = pd.to_datetime(data.index).to_period('D')
     data = data.sort_index(ascending=True)
     stepwise_fit = auto_arima(data['Price'], suppress_warnings=True)
-    stepwise_fit.summary()
+    # stepwise_fit.summary()
     model = ARIMA(data['Price'], order=(1, 0, 5))
     model = model.fit()
-    model.summary()
+    lastDateStr = str(data.index[-1])
+    lastDate = data.index[-1]
+    # model.summary()
+
+
+def predict(date):
+    global model
+    global lastDateStr
+    global lastDate
 
     # last date of train dataset
-    d1 = datetime.strptime(str(data.index[-1]), "%Y-%m-%d")
+    d1 = datetime.strptime(lastDateStr, "%Y-%m-%d")
     # future date
     d2 = datetime.strptime(date, "%Y-%m-%d")
 
     days = d2-d1  # difference between last date of train data and future date
     # prediction of price
     predictions = model.predict(
-        start=data.index[-1] + days.days, end=data.index[-1] + days.days, dynamic=False)
+        start=lastDate + days.days, end=lastDate + days.days, dynamic=False)
+
+    return np.array(predictions)[0]
+
+
+def predictRange(startDate, endDate):
+    global model
+    global lastDateStr
+    global lastDate
+
+    # last date of train dataset
+    d1 = datetime.strptime(lastDateStr, "%Y-%m-%d")
+    # future date
+    d2 = datetime.strptime(startDate, "%Y-%m-%d")
+    d3 = datetime.strptime(endDate, "%Y-%m-%d")
+
+    days = d2-d1  # difference between last date of train data and future date
+    # prediction of price
+    dayRange = d3-d2  # range between start date and end date
+    predictions = model.predict(
+        start=lastDate + days.days, end=lastDate + days.days + dayRange.days, dynamic=False)
+
+    pd.DataFrame(predictions).to_csv("./Prediction/prediction.csv")
+    pdata = pd.read_csv("./Prediction/prediction.csv")
+    pdata.columns = ['Dates', 'USD/INR']
+    pdata.to_csv("./Prediction/prediction.csv")
 
     return str(predictions)
